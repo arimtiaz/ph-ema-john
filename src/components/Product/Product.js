12de28d6c2b3d6de2d9f13a3f19131177a77f301
Abadur Rahman Imtiaz <arimtiaz@Abadurs-MacBook-Pro.local>
2022-08-29 11:24:34 +0600

    working on components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,8 +5,16 @@ const Product = (props) => {
     const {name, img, seller, price, ratings} = props.product;
     return (
         <div className='product'>
-            <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p className>Add to Cart</p>
+            </button>
         </div>
     );
 };
